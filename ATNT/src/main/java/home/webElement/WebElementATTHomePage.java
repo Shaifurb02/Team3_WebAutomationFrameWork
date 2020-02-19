11dec12164e557605f87c41e17bed8d86ed82b4b
Shaifur Bhuiyan <Shaifurr11208@gmail.com>
2020-02-19 11:52:14 -0500

    ATT United

--- ATNT/src/main/java/home/webElement/WebElementATTHomePage.java
+++ ATNT/src/main/java/home/webElement/WebElementATTHomePage.java
@@ -2,9 +2,9 @@ package home.webElement;
 
 import common.WebAPI;
 
-public class WebElementHomePage extends WebAPI {
+public class WebElementATTHomePage extends WebAPI {
 
-
+// ATT webelements
     public static final String xPathClickOnSearchBox = "//input[@id='z1-searchfield']";
     public static final String xPathClickOnMenuBar = "//i[@id='ge5p-menu-active']";
     public static final String xPathClickOnSupport = "//span[@class='z1-support-text']";
@@ -19,18 +19,18 @@ public class WebElementHomePage extends WebAPI {
     public static final String xPathInternetButton = "//li[@id='HEADBAND03']//a[contains(@class,'_20CgK _1hxmk')]";
     public static final String xPathTVButton = "//li[@id='HEADBAND04']//a[contains(@class,'_20CgK _1hxmk')]";
     public static final String xPathClickOnPrePaid = "//li[@id='HEADBAND05']//a[contains(@class,'_20CgK _1hxmk')]";
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//    public static final String xPath
-//
-//
+    public static final String xPathClickOnBundles = "//li[@id='HEADBAND06']//a[@class='_20CgK _1hxmk']";
+    public static final String xPathClickOnShopNow = "//a[contains(@class,'btn-primary mar-t-xs on-dark _2NV38')]";
+    public static final String xPathClickOnLearnMore = "/html[1]/body[1]/div[1]/main[1]/div[4]/div[1]/div[3]/span[1]/a[1]";
+    public static final String xPathClickOnComingSoonLearnMore = "/html[1]/body[1]/div[1]/main[1]/div[5]/div[1]/div[2]/ul[1]/li[1]/div[1]/div[2]/div[2]/span[1]/a[1]";
+    public static final String xPathClickOnAdvantageLearnMore = "/html[1]/body[1]/div[1]/main[1]/div[5]/div[1]/div[2]/ul[1]/li[2]/div[1]/div[2]/div[1]/span[1]/a[1]";
+    public static final String xPathClickOnFiveGForYou = "//a[contains(@class,'_20CgK btn-secondary on-dark btn btn-secondary 31003789-3014-35cd-b3bb-0e11a46d6fc2')]";
+    public static final String xPathProtectAdvantage = "//a[contains(@class,'_20CgK btn-secondary btn btn-secondary 2884bd90-54c8-3f47-813d-e69cc569e57c')]";
+    public static final String xPathATTTV = "//a[@class='_20CgK btn btn-primary-2 btn-arrow d47744b9-f2e7-3936-ae23-0ab336030053']";
+    public static final String xPathAllPhoneDeals = "//a[@class='_20CgK on-dark btn btn-primary mar-b-md btn-arrow 567a1c5d-a7ff-3f0d-ac4d-940f11d5b79b']";
+    public static final String xPathTwoAppleWatches = "//span[contains(text(),'Watches')]";
+    public static final String xPathEnterEmail = "//input[@placeholder='Enter your email']";
+    public static final String xPathConnectDropDownMenu = "//div[@id='SEO_PANEL12ExpandCollapse']";
+
 
 }
